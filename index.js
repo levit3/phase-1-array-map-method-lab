ac538936d813b3870f0b2002aa03e4379f4b8f58
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-04-01 14:37:06 +0300

    adds function with map methods

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = (array) => {
+  let i = array.map((a) => {
+    let j = a.split(" ");
+    let k = j.map((b) => {
+      return b.replace(b[0], b[0].toUpperCase());
+    });
+    return k.join(" ");
+  });
+  console.log(i);
+  return i;
+};
+
+titleCased(tutorials);
